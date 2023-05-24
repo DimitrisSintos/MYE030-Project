b3c508778e3be8c17c04238a4b5668743256041c
thankal <thanasiskal81@gmail.com>
2023-05-25 02:59:35 +0300

    Generate spring boot project

---
+++ web app/countries/src/test/java/mye030/countries/CountriesApplicationTests.java
@@ -0,0 +1,13 @@
+package mye030.countries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

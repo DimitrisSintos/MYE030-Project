43f2ee294508b99871aa4273798f1e6bfb0faeb3
thankal <thanasiskal81@gmail.com>
2023-05-26 20:39:40 +0300

    Setup basic spring project structure

---
+++ web app/countries/src/main/java/mye030/countries/controller/DashboardController.java
@@ -0,0 +1,17 @@
+package mye030.countries.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DashboardController {
+    @RequestMapping("/")
+    public String dashboard(Model model) {
+
+
+        
+        return "dashboard";
+    }
+    
+}

1100715230de3b48d0248eec26021d60e7633b96
Jim <jim@pop-os.localdomain>
2023-05-29 21:23:29 +0300

    front-end:start working on scatter plot

--- web app/countries/src/main/java/mye030/countries/CountriesApplication.java
+++ web app/countries/src/main/java/mye030/countries/CountriesApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CountriesApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		SpringApplication.run(CountriesApplication.class, args);
 	}
 
